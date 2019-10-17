4bcd66a8b8c985270d5d2467407df5df4252a83d
鸡毛 <shihao.gu@perfma.com>
2019-10-17 17:27:13 +0800

    思考如何编写roam

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { App } from 'blockrpg-core/built/SocketIO/App';
 
 const roam = new App('/roam', (client, app) => {
-  console.log(client.Player);
+  client.Socket.broadcast.to('').emit('enter', client.Player.account);
+  // 玩家漫游事件
+  client.Socket.on('roam', (data) => {
+
+  });
+  // 玩家下线事件
+  // 在当前块房间内广播离开漫游消息
+  client.Socket.on('disconnect', () => {
+    client.Socket.broadcast.to('').emit('leave', client.Player.account);
+  });
 }, true);
 
 roam.Listen();
