76af0d28e5697e0e89ac0b987a2056e47af7c873
鸡毛 <shihao.gu@perfma.com>
2019-10-17 17:57:02 +0800

    roam纠结,如何实现roam

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ const roam = new App('/roam', (client, app) => {
   client.Socket.broadcast.to('').emit('enter', client.Player.account);
   // 玩家漫游事件
   client.Socket.on('roam', (data) => {
-
+    const x = data.x;
+    const y = data.y;
+    const dir = data.dir;
+    const ges = data.ges;
   });
   // 玩家下线事件
   // 在当前块房间内广播离开漫游消息
