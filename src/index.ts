e89d678fffde45bf110ec453d763f3ca862a0d5f
鸡毛 <shihao.gu@perfma.com>
2019-10-18 11:16:48 +0800

    构思如何实现服务

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,46 @@
 import { App } from 'blockrpg-core/built/SocketIO/App';
+import { Roamer } from 'blockrpg-core/built/Model/Roamer/Entity';
+import * as RoamerBLL from 'blockrpg-core/built/Model/Roamer/BLL';
 
-const roam = new App('/roam', (client, app) => {
-  client.Socket.broadcast.to('').emit('enter', client.Player.account);
-  // 玩家漫游事件
-  client.Socket.on('roam', (data) => {
-    const x = data.x;
-    const y = data.y;
-    const dir = data.dir;
-    const ges = data.ges;
+const roam = new App('/roam', async (client, app) => {
+
+  let curRoamer: Roamer = await RoamerBLL.getRoamerBLL(client.Player.account) as Roamer;
+
+  // 玩家进入事件
+  client.Socket.on('enter', async () => {
+
   });
-  // 玩家下线事件
+
+  // 玩家漫游事件
+  client.Socket.on('roam', async (data) => {
+    // 构建漫步者对象
+    const roamer = new Roamer({
+      account: client.Player.account,
+      x: data.x,
+      y: data.y,
+      dir: data.dir,
+      ges: data.ges,
+    });
+    // 缓存漫步者对象到Redis
+    await RoamerBLL.updateRoamerBLL(roamer);
+    const newBlocks = roamer.CurBlockPoint.Nine;
+    const oldBlocks = curRoamer.CurBlockPoint.Nine;
+    const joinBlocks = newBlocks.filter(pt => oldBlocks.every(opt => opt.Id !== pt.Id));
+    const leaveBlocks = oldBlocks.filter(pt => newBlocks.every(npt => npt.Id !== pt.Id));
+    client.Socket.join(joinBlocks.map(block => block.Id));
+    leaveBlocks.forEach(block => {
+      client.Socket.leave(block.Id);
+    });
+    curRoamer = roamer;
+    client.Socket.broadcast.to(curRoamer.CurBlockPoint.Id).emit('roam', {
+
+    });
+  });
+
+  // 玩家离开事件
   // 在当前块房间内广播离开漫游消息
-  client.Socket.on('disconnect', () => {
+  client.Socket.on('disconnect', async () => {
+    await RoamerBLL.syncRoamerBLL(client.Player.account);
     client.Socket.broadcast.to('').emit('leave', client.Player.account);
   });
 }, true);
